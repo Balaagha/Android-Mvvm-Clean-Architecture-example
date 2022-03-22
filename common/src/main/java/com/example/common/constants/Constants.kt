a7187c885f1263ced750813e659efe7416af8355
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-03-22 14:13:36 +0400

    add common Constants.kt

---
+++ common/src/main/java/com/example/common/constants/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.common.constants
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
+        const val API_KEY = "121e0c6ca2b047ea96baf3c27c86cefa"
+    }
+
+}
+
+object Bundles {
+    const val RECIPE_RESULT_KEY = "recipeResultBundle"
+}
