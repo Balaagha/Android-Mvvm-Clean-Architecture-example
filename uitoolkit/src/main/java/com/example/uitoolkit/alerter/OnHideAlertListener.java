3216d4bad220bfb7eb43265f0f69d0822ceb3230
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-06-09 12:09:14 +0400

    add alerter component

---
+++ uitoolkit/src/main/java/com/example/uitoolkit/alerter/OnHideAlertListener.java
@@ -0,0 +1,14 @@
+package com.example.uitoolkit.alerter;
+
+/**
+ * On Hide Alert Listener
+ *
+ * @author edgeorge
+ * @since 20/02/2017.
+ */
+public interface OnHideAlertListener {
+    /**
+     * Called when the Alert is hidden
+     */
+    void onHide();
+}
---
+++ uitoolkit/src/main/java/com/example/uitoolkit/alerter/OnShowAlertListener.java
@@ -0,0 +1,12 @@
+package com.example.uitoolkit.alerter;
+
+/**
+ * On Show Alert Listener
+ *
+ */
+public interface OnShowAlertListener {
+    /**
+     * Called when the Alert is initially Shown
+     */
+    void onShow();
+}
