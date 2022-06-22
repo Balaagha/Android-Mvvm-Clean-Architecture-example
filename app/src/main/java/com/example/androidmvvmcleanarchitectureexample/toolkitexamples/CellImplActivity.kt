bbe0bd49abcee50225d224a104712c5369e1c5eb
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-06-22 20:14:40 +0400

    add cell examples

---
+++ app/src/main/java/com/example/androidmvvmcleanarchitectureexample/toolkitexamples/CellImplActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidmvvmcleanarchitectureexample.toolkitexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmvvmcleanarchitectureexample.R
+
+class CellImplActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cell_impl)
+    }
+}
